ce0f4f156574a0bb3d6f410cf0b718be5d8e10f9
DeeAnnEye <danielashulamithjustin@gmail.com>
2021-04-17 06:14:19 +0530

    GLobal state 'task'const tasks =[

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text:'Shopping',
+        day:'12th April at 6:30pm',
+        reminder:true
+    },
+    {
+        id:2,
+        text:'Dentist appointment',
+        day:'24th April at 11:00am',
+        reminder:true
+    },
+    {
+        id:3,
+        text:'Birthday party',
+        day:'28th April at 7:00pm',
+        reminder:true
+    },
+    {
+        id:4,
+        text:'School meeting',
+        day:'29th April at 10:00am',
+        reminder:true
+    }
+])
+
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={ tasks }/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,4 @@
-const tasks =[
-    {
-        id:1,
-        text:'Shopping',
-        day:'12th April at 6:30pm',
-        reminder:true
-    },
-    {
-        id:2,
-        text:'Dentist appointment',
-        day:'24th April at 11:00am',
-        reminder:true
-    },
-    {
-        id:3,
-        text:'Birthday party',
-        day:'28th April at 7:00pm',
-        reminder:true
-    },
-    {
-        id:4,
-        text:'School meeting',
-        day:'29th April at 10:00am',
-        reminder:true
-    }
-]
-
-const Tasks = () => {
+const Tasks = ({tasks}) => {
     return (
         <>
          {tasks.map((task) => (<h3>{task.text}</h3>)
